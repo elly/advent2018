6415ae1dcc3ef5e3af96127f21157c563991c66e
Elly Fong-Jones <elly@leptoquark.net>
2018-12-24 14:53:18 -0500

    9b & rework of 9a

--- p9.scm
+++ p9.scm
@@ -4,20 +4,43 @@
 (require "base.scm")
 (provide (all-defined-out))
 
-(struct cvec (vec index) #:transparent)
+(struct cvec (val prev next) #:transparent #:mutable)
+
+(define (cvec-make)
+  (let ((cv (cvec 0 #f #f)))
+    (set-cvec-next! cv cv)
+    (set-cvec-prev! cv cv)
+    cv))
+
+(define (cvec-fwd cv n)
+  (let loop ((cv cv) (n n))
+    (if (= 0 n)
+      cv
+      (loop (cvec-next cv) (sub1 n)))))
+
+(define (cvec-back cv n)
+  (let loop ((cv cv) (n n))
+    (if (= 0 n)
+      cv
+      (loop (cvec-prev cv) (sub1 n)))))
 
 (define (cvec-insert cv iv)
-  (let* ((i (modulo (+ (cvec-index cv) 2) (length (cvec-vec cv))))
-         (left (take (cvec-vec cv) i))
-         (right (drop (cvec-vec cv) i)))
-    (cvec (append left (list iv) right) i)))
+  (let* ((np (cvec-fwd cv 1))
+         (nn (cvec-fwd cv 2))
+         (ncv (cvec iv np nn)))
+    (set-cvec-next! np ncv)
+    (set-cvec-prev! nn ncv)
+    ncv))
+
+(define (cvec-unlink! cv)
+  (set-cvec-next! (cvec-prev cv) (cvec-next cv))
+  (set-cvec-prev! (cvec-next cv) (cvec-prev cv)))
 
 (define (cvec-remove cv)
-  (let* ((i (modulo (- (cvec-index cv) 7) (length (cvec-vec cv))))
-         (left (take (cvec-vec cv) i))
-         (right (drop (cvec-vec cv) (+ i 1))))
-    (values (cvec (append left right) i)
-            (list-ref (cvec-vec cv) i))))
+  (let ((t (cvec-back cv 7)))
+    (cvec-unlink! t)
+    (values (cvec-next t)
+            (cvec-val t))))
 
 ; (0)
 ; 0 (1)
@@ -30,7 +53,7 @@
 
 (define (run-game np nm)
   (let ((scores (make-vector np 0)))
-    (let loop ((g (cvec '(0) 0)) (turn 0) (marble 1))
+    (let loop ((g (cvec-make)) (turn 0) (marble 1))
       (if (> marble nm)
         scores
         (if (special-marble? marble)
@@ -56,6 +79,8 @@
       id
       (run-game np nm))))
 
-(define (try-some-stuff)
-  (for ((i 50))
-    (printf "~a: ~a~n" i (vector-argmax id (run-game 9 (* 23 i))))))
+(define (part-b fn)
+  (let-values (((np nm) (parse fn)))
+    (vector-argmax
+      id
+      (run-game np (* 100 nm)))))
